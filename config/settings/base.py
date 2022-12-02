b00499607279d80f59fbcbfcdb2f99c4c0627bc8
Leonel Ubeda <leonelubeda@hotmail.com>
2022-12-01 23:42:00 -0600

    add pagination config

--- config/settings/base.py
+++ config/settings/base.py
@@ -278,6 +278,8 @@ SOCIALACCOUNT_FORMS = {"signup": "portal.users.forms.UserSocialSignupForm"}
 # -------------------------------------------------------------------------------
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
